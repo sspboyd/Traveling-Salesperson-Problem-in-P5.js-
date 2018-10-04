9fafc0f189fdfc0f6b255aef82d0198e454498d8
sspboyd <sspboyd@gmail.com>
2018-10-04 11:58:47 -0400

    Finished. Added behaviour for each lexigraphic ordering to be run. Some UI to indicate how far through the solution. Lots of interesting visual updates that can be made.

--- sketch.js
+++ sketch.js
@@ -1,61 +1,27 @@
-let vals = [0, 4,23,3,457];
+let order = [];
 
 
 
 let cities = [];
-let totalCities = 11;
+let totalCities = 7;
 let shortestDistOrder = [];
 let shortestDist = Infinity;
 let shortestDistScores = [];
-
+let totalPerms = fact(totalCities);
+let permCount=1;
 
 function setup() {
-    createCanvas(400, 300);
+    createCanvas(400, 800);
     for (i = 0; i < totalCities; i++) {
         let v = createVector(Math.random() * width, Math.random() * height);
         cities[i] = v;
+        order[i] = i;
     }
-
+    console.log("order: " + order);
 }
 
 function draw() {
     background(0);
-    console.log(vals);
-    let largestI = -1;
-    for (var i = 0; i < vals.length - 1; i++) {
-        if (vals[i] < vals[i + 1]) {
-            largestI = i;
-        }
-    }
-
-    if (largestI === -1) {
-        noLoop();
-        console.log('finished');
-    }
-
-    // Lex ordering Step 2
-    let largestJ = -1;
-    for (var j = 0; j < vals.length; j++) {
-        if (vals[largestI] < vals[j]) {
-            largestJ = j;
-        }
-    }
-
-
-
-
-
-    // Lex ordering Step 3
-    swap(vals, largestI, largestJ);
-
-    let endArray = vals.splice(largestI + 1);
-    endArray.reverse();
-    vals = vals.concat(endArray);
-
-
-
-
-
 
     cities.forEach(function(c, index) {
         noStroke();
@@ -66,7 +32,7 @@ function draw() {
     beginShape();
     stroke(199);
     strokeWeight(.5);
-    noFill();
+    fill(199,18);
     for (let i = 0, length1 = cities.length; i < length1; i++) {
         vertex(cities[i].x, cities[i].y);
     }
@@ -94,7 +60,22 @@ function draw() {
     text("Current Shortest Distance: " + shortestDist, 10, 10);
     text("Prev Best Scores: " + shortestDistScores, 10, 25, width - 10, height - 35);
 
-    cities = randomizeArray(cities);
+    // cities = randomizeArray(cities);
+    for (let i = 0, length1 = order.length; i < length1; i++) {
+        swap(cities, i, order[i]);
+    }
+
+    let s = "order: ";
+    for (let i = 0; i < order.length; i++) {
+        s += order[i];
+    }
+    fill(199);
+    // console.log("s: "+s);
+    // let percentComplete =
+    text((permCount/totalPerms*100).toFixed(3) + "% complete", 10, height - 50);
+
+    permCount++;
+    nextOrder();
 }
 
 
@@ -123,4 +104,47 @@ function calcDist(arr) {
         d += Math.floor(arr[i].dist(arr[i + 1]));
     }
     return d;
+}
+
+
+// lexical ordering algorithm
+function nextOrder() {
+    let largestI = -1;
+    for (var i = 0; i < order.length - 1; i++) {
+        if (order[i] < order[i + 1]) {
+            largestI = i;
+        }
+    }
+
+    if (largestI === -1) {
+        noLoop();
+        console.log('finished');
+    }
+
+    // Lex ordering Step 2
+    let largestJ = -1;
+    for (var j = 0; j < order.length; j++) {
+        if (order[largestI] < order[j]) {
+            largestJ = j;
+        }
+    }
+
+
+
+    // Lex ordering Step 3
+    swap(order, largestI, largestJ);
+
+    let endArray = order.splice(largestI + 1);
+    endArray.reverse();
+    order = order.concat(endArray);
+}
+
+
+
+function fact(num) {
+    let f = 1;
+    for (var i = 2; i <= num; i++) {
+        f = f * i;
+    }
+    return f;
 }
